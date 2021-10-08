97f91776fd77eea51efb9b56745ab876937a6188
Sakshi <sakshishetty1231997@gmail.com>
2021-10-08 16:41:22 +0530

    Added: Get request method to print message

---
+++ src/main/java/com/bridgelabz/restapicalls/RestApiCallsController.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.restapicalls;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RestApiCallsController {
+
+    @GetMapping("test1")
+    public String getMessage(){
+        return "Hello from Bridgelabz";
+    }
+}
